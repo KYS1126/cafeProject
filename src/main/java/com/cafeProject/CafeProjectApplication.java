ab71cb90ac1e120831a15e89637772af2549082e
KYS1126 <kimyongsu1226@naver.com>
2023-03-06 20:50:58 +0900

    up

--- src/main/java/com/cafeProject/CafeProjectApplication.java
+++ src/main/java/com/cafeProject/CafeProjectApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CafeProjectApplication { 
+public class CafeProjectApplication {   
   
 	public static void main(String[] args) {        
 		SpringApplication.run(CafeProjectApplication.class, args);
